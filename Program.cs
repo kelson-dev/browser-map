5de3757ca44799795cf4ae1a9cbc42b0e7c68935
kelson <kelson.j.ball@gmail.com>
2022-06-20 15:15:45 -0700

    Add project

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Text.RegularExpressions;
+using System.Web;
+using CliWrap;
+
+var config_path = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/browser-intercept.config";
+var (command, path) = FindConfiguredBrowserSelection(args);
+if (command != "") // empty string indicates no browser was found given the configuration
+    await Cli.Wrap(command).WithArguments(path).ExecuteAsync();
+
+(string command, string uri) FindConfiguredBrowserSelection(IReadOnlyList<string> args)
+{
+    if (args.Count == 0) // if no args, don't
+        return ("", "");
+    if (!File.Exists(config_path)) // if no config, don't
+        return ("", args[0]);
+    
+    var config_state = new ConfigState();
+    
+    var nav = new Uri(args[0]);
+    using (var stream = File.OpenRead(config_path))
+        using (var reader = new StreamReader(stream))
+            while (reader.ReadLine() is { Length: > 0 } line)
+                if (ConfigRow.TryParse(line, out var row))
+                    row.Update(ref config_state, nav);
+
+    var query_params = HttpUtility.ParseQueryString(nav.Query);
+    var selected_query_params = HttpUtility.ParseQueryString("");
+    foreach (var key in query_params.AllKeys!)
+    {
+        if (key is null)
+            continue;
+        var included = true;
+        foreach (var (include, match) in config_state.QueryParamExclusions)
+            if (match.IsMatch(key))
+                included = include;
+        if (included)
+            selected_query_params.Add(key, query_params.Get(key));
+    }
+
+    var builder = new UriBuilder
+    {
+        Scheme = nav.Scheme,
+        Host = nav.Host,
+        Path = nav.AbsolutePath,
+        Query = selected_query_params.ToString()
+    };
+
+    return (config_state.SelectedBrowser, builder.Uri.ToString());
+}
+
+enum ConfigAction
+{
+    /// <summary>
+    /// Maps hosts ending in [domain] to [command]
+    /// May be overridden by subsequent configurations.
+    /// Example:
+    /// |map|.*google.com|/dev/null
+    /// </summary>
+    map,
+    /// <summary>
+    /// Indicates that query params whose keys match the given patterns should be excluded for the specified domain.
+    /// May be overridden by subsequent configurations.
+    /// Example:
+    /// |exclude_query|.*twitter.com|^s$|^t$
+    /// </summary>
+    exclude_query,
+    /// <summary>
+    /// Indicates that query params whose keys match the given patterns should be included for the specified domain, even if previously excluded.
+    /// May be overridden by subsequent configurations.
+    /// Example:
+    /// |exclude_query|.*twitter.com|^s$|^t$
+    /// </summary>
+    include_query
+}
+
+/// <summary>
+/// Represents a configuration applied to a specific url
+/// </summary>
+public struct ConfigState
+{
+    public string SelectedBrowser = "";
+    public readonly List<(bool include, Regex match)> QueryParamExclusions;
+
+    public ConfigState() => QueryParamExclusions = new();
+}
+
+record struct ConfigRow(char Delimiter, ConfigAction Action, Regex Domain, string[] Parameters)
+{
+    public void Update(ref ConfigState state, Uri uri)
+    {
+        if (!Domain.IsMatch(uri.Host))
+            return;
+        switch (Action)
+        {
+            case ConfigAction.map:
+                foreach (var p in Parameters)
+                {
+                    if (!File.Exists(p)) continue;
+                    state.SelectedBrowser = p;
+                    return;
+                }
+                return;
+            case ConfigAction.exclude_query:
+                foreach (var p in Parameters)
+                    state.QueryParamExclusions.Add((false, new Regex(p)));
+                return;
+            case ConfigAction.include_query:
+                foreach (var p in Parameters)
+                    state.QueryParamExclusions.Add((true, new Regex(p)));
+                return;
+            default:
+                return;
+        }
+    }
+
+    public static bool TryParse(string line, out ConfigRow row)
+    {
+        row = default;
+        if (line.Length == 0)
+            return false;
+        var delimiter = line[0];
+        var remaining = line.AsSpan()[1..];
+        Queue<string> values = new();
+        while (remaining.Length > 0)
+        {
+            var index = remaining.IndexOf(delimiter);
+            var content = index < 0 ? remaining : remaining[..index];
+            values.Enqueue(content.ToString());
+            remaining = remaining[content.Length..].TrimStart(delimiter);
+        }
+        if (values.Count < 3)
+            return false;
+
+        if (!Enum.TryParse(values.Dequeue(), out ConfigAction action))
+            return false;
+
+        row = new(delimiter, action, new Regex(values.Dequeue()), values.ToArray());
+        return true;
+    }
+};
+
--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # browser-map
+
+This is a terminal application you can set as your "default browser" in order to configure how links get opened in different browsers.
+
+# Installation
+
+Create a config file (described next) and build the application (described after configuration)
+
+After setting up a configuration and building you can set your computers "default browser" to the built program using the settings of your desktop environment.
+
+
+# Configuration
+
+The application will read a config from a `browser-intercept.config` file in your "ApplicationData" directory. For me this is at `~/.config/browser-intercept.config`
+
+Example `browser-intercept.config` to map all links except for twitter links to Firefox, and twitter links Brave:
+```
+|map|.*|/snap/bin/firefox
+|map|^(www\.)?twitter\.com|/bin/brave-browser
+```
+
+The configuration format has a configuration on each line. The first character defines the seperator for that line. `|` is used here but if you need that character you can use any other character as the delimiter. The second value is what configuration action to take. The third value is a regex that defines which domains/hosts that configuration action applies to. All remaining values are parameters to the configuration action. Configuration actions override any settings specified previously.
+
+The available actions are currently `map`, `exclude_query`, and `include_query`.
+
+### Action: map
+
+Maps domains to a specific browser by specifying a domain as a regex and a terminal command to execute to run the browser.
+
+Format: `|map|domain regex|browser shell command`
+
+Example (map google services to chromium): 
+```
+|map|^(www.\)?google.com|/bin/chromium
+```
+
+### Action: exclude_query
+
+Specifies query parameters to exclude when opening links.
+
+Format: `|exclude_query|domain regex|filter regex|filter regex|....`
+
+Example (remove `s` and `t` tracker params from twitter links): 
+```
+|exclude_query|^(www\.)?twitter\.com|^s$|^t$
+```
+
+### Action: include_query
+
+Specifies query parameters to include when opening links. (Overrides previous exclusions)
+
+Format: `|include_query|domain regex|selection regex|selection regex|....`
+
+Example (remove all but the `q` search term param from google searches): 
+```
+|exclude_query|^(www\.)?google\.com$|.*
+|include_query|^(www\.)?google\.com$|^q$
+```
+
+# Building
+
+Building the project requires the [.NET 6.0 sdk](https://dotnet.microsoft.com/en-us/download/dotnet/6.0) to be installed. Future versions will probably work.
+
+(To disable telemetry create an environment variable `DOTNET_CLI_TELEMETRY_OPTOUT` with value `1`)
+
+All commands should be ran from the directory containing `browser-map.csproj`
+
+
+
+### Basic build for systems with the runtime installed, will output to `./bin/Debug/net6.0/`
+```
+dotnet build
+```
+
+### Run from source with an URL
+```
+dotnet run https://google.com/search?q=example+search
+```
+
+### Publish to a single file (or very few files) 
+
+For linux (output to `./bin/Release/net6.0/linux-x64/publish/`):
+```
+dotnet publish -c Release --self-contained -r linux-x64
+```
+
+For windows (output to `./bin/Release/net6.0/win-x64/publish/`):
+```
+dotnet publish -c Release --self-contained -r win-x64
+```
+
+If you would like a smaller executable and don't mind the runtime needing to stay installed you can use `--no-self-contained` instead of `--self-contained`.
